c49e5a07531cf6a43f4da4481485a226095b51e6
CHILL <chillbrodev444@gmail.com>
2024-01-22 18:30:13 +0530

    added new site

---
+++ domains/chillcodes.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+    description: "Just a simple portofolio", // describe your project in this field
+    domain: "is-an.app", // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+    subdomain: "chillcodes", // desired subdomain name
+    owner: {
+      repo: "https://github.com/CHILLBRO28/chillcode",
+      email: "chillbrodev444@gmail.com",
+    },
+    
+    record: {
+      CNAME: "CHILLBRO28.github.io", // e.g.: <your-github-account>.github.io
+    },
+  });
+  
